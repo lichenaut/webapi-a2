59459dfe8ba6edc7ffc697fe36b214a228042b1e
lichenaut <mt0n@tutanota.com>
2025-06-07 20:41:24 -0600

    commit

---
+++ readme.md
@@ -0,0 +1,5 @@
+Shareable: https://webapi-class.postman.co/workspace/webapi-workspace~25d20f53-9c9a-468f-a6f1-151ff32ba87c/collection/34573647-e13e8ea9-0e2f-4c82-b5de-8c5f9ec3fff5?action=share&creator=34573647&active-environment=34573647-0a610ce4-fb6c-403b-9a3a-522fe9811800
+
+Make sure to select the "lichenaut-hw2" environment, which has the key for info purposes.
+
+Collection is named "CSCI3916_HW2_LICHENAUT"
---
+++ server.js
@@ -0,0 +1,152 @@
+/*
+CSC3916 HW2
+File: Server.js
+Description: Web API scaffolding for Movie API
+ */
+
+var express = require("express");
+var http = require("http");
+var bodyParser = require("body-parser");
+var passport = require("passport");
+var authController = require("./auth");
+var authJwtController = require("./auth_jwt");
+db = require("./db")(); //hack
+var jwt = require("jsonwebtoken");
+var cors = require("cors");
+
+var app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(passport.initialize());
+
+var router = express.Router();
+
+function getJSONObjectForMovieRequirement(req) {
+  var json = {
+    headers: "No headers",
+    key: process.env.SECRET_KEY,
+    body: "No body",
+  };
+
+  if (req.body != null) {
+    json.body = req.body;
+  }
+
+  if (req.headers != null) {
+    json.headers = req.headers;
+  }
+
+  return json;
+}
+
+router.post("/signup", (req, res) => {
+  if (!req.body.username || !req.body.password) {
+    res.json({
+      success: false,
+      msg: "Please include both username and password to signup.",
+    });
+  } else {
+    var newUser = {
+      username: req.body.username,
+      password: req.body.password,
+    };
+
+    db.save(newUser); //no duplicate checking
+    res.json({ success: true, msg: "Successfully created new user." });
+  }
+});
+
+router.post("/signin", (req, res) => {
+  var user = db.findOne(req.body.username);
+
+  if (!user) {
+    res
+      .status(401)
+      .send({ success: false, msg: "Authentication failed. User not found." });
+  } else {
+    if (req.body.password == user.password) {
+      var userToken = { id: user.id, username: user.username };
+      var token = jwt.sign(userToken, process.env.SECRET_KEY);
+      res.json({ success: true, token: "JWT " + token });
+    } else {
+      res.status(401).send({ success: false, msg: "Authentication failed." });
+    }
+  }
+});
+
+router
+  .route("/testcollection")
+  .delete(authController.isAuthenticated, (req, res) => {
+    console.log(req.body);
+    res = res.status(200);
+    if (req.get("Content-Type")) {
+      res = res.type(req.get("Content-Type"));
+    }
+    var o = getJSONObjectForMovieRequirement(req);
+    res.json(o);
+  })
+  .put(authJwtController.isAuthenticated, (req, res) => {
+    console.log(req.body);
+    res = res.status(200);
+    if (req.get("Content-Type")) {
+      res = res.type(req.get("Content-Type"));
+    }
+    var o = getJSONObjectForMovieRequirement(req);
+    res.json(o);
+  });
+
+router
+  .route("/movies")
+  .get((req, res) => {
+    var o = getJSONObjectForMovieRequirement(req);
+    o.status = 200;
+    o.message = "GET movies";
+    o.headers = req.headers;
+    o.query = req.query;
+    o.env = process.env.SECRET_KEY;
+    res.json(o);
+  })
+  .post((req, res) => {
+    var o = getJSONObjectForMovieRequirement(req);
+    o.status = 200;
+    o.message = "movie saved";
+    o.headers = req.headers;
+    o.query = req.query;
+    o.env = process.env.SECRET_KEY;
+    res.json(o);
+  })
+  .put(authJwtController.isAuthenticated, (req, res) => {
+    // HTTP PUT Method
+    // Requires JWT authentication.
+    // Returns a JSON object with status, message, headers, query, and env.
+    var o = getJSONObjectForMovieRequirement(req);
+    o.status = 200;
+    o.message = "movie updated";
+    o.headers = req.headers;
+    o.query = req.query;
+    o.env = process.env.SECRET_KEY;
+    res.json(o);
+  })
+  .delete(authController.isAuthenticated, (req, res) => {
+    // HTTP DELETE Method
+    // Requires Basic authentication.
+    // Returns a JSON object with status, message, headers, query, and env.
+    var o = getJSONObjectForMovieRequirement(req);
+    o.status = 200;
+    o.message = "movie deleted";
+    o.headers = req.headers;
+    o.query = req.query;
+    o.env = process.env.SECRET_KEY;
+    res.json(o);
+  })
+  .all((req, res) => {
+    // Any other HTTP Method
+    // Returns a message stating that the HTTP method is unsupported.
+    res.status(405).send({ message: "HTTP method not supported." });
+  });
+
+app.use("/", router);
+app.listen(process.env.PORT || 8080);
+module.exports = app; // for testing only
